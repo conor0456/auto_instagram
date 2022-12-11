a457bf3d5ed1fcfe5c71bdd719559afbfd086238
Conor D'Arcy <conordarcy@Conors-MacBook-Air.local>
2022-12-11 21:33:44 +0000

    able to post to instagram

--- auto_instagram/instagram/instagram_client.py
+++ auto_instagram/instagram/instagram_client.py
@@ -1,28 +1,30 @@
-import requests
-import json
+from pyfacebook import GraphAPI
 from decouple import config
 
-def generate_token():
-    response = requests.get("https://graph.facebook.com/v15.0/oauth/access_token",
-        params={
-            "grant_type": "fb_exchange_token",
-            "client_id": config('INSTAGRAM_CLIENT_ID'),
-            "client_secret": config('INSTAGRAM_CLIENT_SECRET'),
-            "fb_exchange_token": config('INSTAGRAM_GENERATED_TOKEN')
-        })
-    json_data = json.loads(response.text)
-    print(json_data['access_token'])
-    return json_data['access_token']
+ACCESS_TOKEN =
+INSTAGRAM_BUSINESS_ID = config('INSTAGRAM_USER_ID')
 
-def generate_post():
-    url = f"https://graph.facebook.com/v15.0/{config('INSTAGRAM_USER_ID')}/media"
-    print(url)
-    payload = {
-        'image_url': 'https://media.nga.gov/iiif/a8c923e1-078d-4f94-b1f4-0e303afe2155__640/full/!740,560/0/default.jpg',
-        'caption': 'A real Rothko',
-        'access_token': config('INSTAGRAM_GENERATED_TOKEN')
-    }
-    r = requests.post(url, data=payload)
-    print(r.text)
+api = GraphAPI(access_token=config('INSTAGRAM_GENERATED_TOKEN'))
 
-generate_post()
+data = api.post_object(
+    object_id=INSTAGRAM_BUSINESS_ID,
+    connection="media",
+    params={
+        "image_url": "https://media.nga.gov/iiif/a8c923e1-078d-4f94-b1f4-0e303afe2155__640/full/!740,560/0/default.jpg",
+        "caption": "Image by api",
+    },
+)
+print(data)
+# {'id': '17952987976782688'}
+# Get your container id.
+container_id = data["id"]
+
+# Then publish the container.
+publish_data = api.post_object(
+    object_id=INSTAGRAM_BUSINESS_ID,
+    connection="media_publish",
+    params={
+        "creation_id": container_id,
+    },
+)
+print(publish_data)
---
+++ auto_instagram/instagram/request_based_instagram_client.py
@@ -0,0 +1,28 @@
+import requests
+import json
+from decouple import config
+
+def generate_token():
+    response = requests.get("https://graph.facebook.com/v15.0/oauth/access_token",
+        params={
+            "grant_type": "fb_exchange_token",
+            "client_id": config('INSTAGRAM_CLIENT_ID'),
+            "client_secret": config('INSTAGRAM_CLIENT_SECRET'),
+            "fb_exchange_token": config('INSTAGRAM_GENERATED_TOKEN')
+        })
+    json_data = json.loads(response.text)
+    print(json_data['access_token'])
+    return json_data['access_token']
+
+def generate_post():
+    url = f"https://graph.facebook.com/v15.0/{config('INSTAGRAM_USER_ID')}/media"
+    print(url)
+    payload = {
+        'image_url': 'https://media.nga.gov/iiif/a8c923e1-078d-4f94-b1f4-0e303afe2155__640/full/!740,560/0/default.jpg',
+        'caption': 'A real Rothko',
+        'access_token': config('INSTAGRAM_GENERATED_TOKEN')
+    }
+    r = requests.post(url, data=payload)
+    print(r.text)
+
+generate_token()
