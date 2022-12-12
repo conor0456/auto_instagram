094264a44e8a7b178e133aec09c9d0c9cc83356e
Conor D'Arcy <conordarcy@Conors-MacBook-Air.local>
2022-12-12 00:20:16 +0000

    hash tags

--- auto_instagram/instagram/helper.py
+++ auto_instagram/instagram/helper.py
@@ -1,4 +1,8 @@
 from decouple import config
 
+HASHTAGS = ['#ai', '#aiart', '#aiartwork', '#aiartists', '#aiartcommunity', '#aiartdaily', '#aiartist']
+
+
 def build_caption(title, image_generation_prompt, summary):
-    return f"{title} \n\n Image generated with the prompt: {image_generation_prompt} \n\n {summary}"
+    hashtags = " ".join(HASHTAGS)
+    return f"{title} \n\nImage generated with the prompt: {image_generation_prompt} \n\n{summary} \n\n{hashtags}"
