531280ee3a0c6a7eef013bc9fddf7cf0a113b347
Conor D'Arcy <conordarcy@Conors-MacBook-Air.local>
2022-12-14 08:46:26 +0000

    google drive working from macbook

---
+++ auto_instagram/persistence/google_drive_client.py
@@ -0,0 +1,50 @@
+from __future__ import print_function
+
+import google.auth
+import os
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from googleapiclient.http import MediaFileUpload
+from google.oauth2.credentials import Credentials
+from decouple import config
+import requests
+
+SCOPES = ['https://www.googleapis.com/auth/drive']
+AUTHORIZATION_URL = "https://oauth2.googleapis.com/token"
+FOLDER_ID = config('GOOGLE_DRIVE_IMAGE_DIRECTORY_ID')
+CREDENTIALS_FILE_NAME = config('GOOGLE_DRIVE_CREDENTIALS_FILE_NAME')
+TOKEN_FILE_NAME = config('GOOGLE_DRIVE_TOKEN_FILE_NAME')
+dir = os.path.dirname(__file__)
+
+def upload_file_to_drive(file_name):
+    try:
+        access_token = generate_token()
+        creds = google.oauth2.credentials.Credentials(access_token)
+        service = build('drive', 'v3', credentials=creds)
+        file_metadata = {
+            'name': file_name,
+            'parents': [FOLDER_ID]
+        }
+        media = MediaFileUpload(os.path.join(dir, '..','..','images', file_name),
+                                mimetype='image/jpg')
+        print(f"Uploading file {file_name} to google drive")
+        file = service.files().create(body=file_metadata, media_body=media,
+                                      fields='id').execute()
+        print("successfully uploaded file to google drive")
+    except Exception as e:
+        print(f"Encountered error: {e} when attempting to save to google drive, skipping for now")
+
+def generate_token():
+    params = {
+            "grant_type": "refresh_token",
+            "client_id": config('GOOGLE_DRIVE_CLIENT_ID'),
+            "client_secret": config('GOOGLE_DRIVE_CLIENT_SECRET'),
+            "refresh_token": config('GOOGLE_DRIVE_CLIENT_REFRESH_TOKEN')
+    }
+
+    r = requests.post(AUTHORIZATION_URL, data=params)
+    if r.ok:
+        print("Generated auth token for google drive")
+        return r.json()['access_token']
+    else:
+        return None
--- auto_instagram/run.py
+++ auto_instagram/run.py
@@ -3,7 +3,8 @@ import instagram.helper as helper
 import instagram.client as instagram_client
 import prompt_generator.generate_artist as artist_generator
 import image_upscaling.cv2_client as upscaler
-import persistence.local_storage as storage
+import persistence.local_storage as local_storage
+import persistence.google_drive_client as google_storage
 
 subject = artist_generator.fetch_random_artist()
 image_generation_prompt = ai_client.generate_image_generation_prompt(subject)
@@ -15,6 +16,7 @@ instagram_caption = helper.build_caption(title, image_generation_prompt, summary
 instagram_client.post_file_to_instagram_with_caption(image_url, instagram_caption)
 file_name = ai_client.generate_file_name(image_generation_prompt, subject)
 upscaler.upscale_and_overwrite_image(file_name)
-storage.save_entry(subject, title, summary, file_name)
+local_storage.save_content(subject, title, summary, file_name)
+google_storage.upload_file_to_drive(file_name)
 
-print(f"Completed task, wrote the following caption: {instagram_caption}")
+print(f"Completed all tasks")
