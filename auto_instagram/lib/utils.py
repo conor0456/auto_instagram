ab7c438611b020dee3395fc2198d1500fd19c387
Conor D'Arcy <conordarcy@Conors-MacBook-Air.local>
2022-12-17 20:03:18 +0000

    better configs

---
+++ auto_instagram/lib/utils.py
@@ -0,0 +1,37 @@
+from decouple import config
+import os
+
+dir = os.path.dirname(__file__)
+
+def print_run_configs():
+    print(f"""
+Running script with configs:
+Post To Instagram: {should_post_to_instagram()}
+Upscale Image: {should_upscale_image()}
+Persist Image Locally: {should_persist_image_locally()}
+Persist image Remotely: {should_persist_image_remotely()}
+    """)
+
+def should_post_to_instagram():
+    return read_bool_config('POST_TO_INSTAGRAM')
+
+def should_upscale_image():
+    return read_bool_config('UPSCALE_IMAGE')
+
+def should_persist_image_locally():
+    return read_bool_config('PERSIST_IMAGES_LOCALLY')
+
+def should_persist_image_remotely():
+    return read_bool_config('PERSIST_IMAGES_REMOTELY')
+
+def read_bool_config(config_name):
+    value = config(config_name)
+    if value is None:
+        return False
+    else:
+        return value.strip().lower() == 'true'
+
+def clean_up_local_image(file_name):
+    print('Deleting local file')
+    path = os.path.join(dir, '..','..','images', file_name)
+    os.remove(path)
--- auto_instagram/run.py
+++ auto_instagram/run.py
@@ -5,18 +5,30 @@ import prompt_generator.generate_artist as artist_generator
 import image_upscaling.cv2_client as upscaler
 import persistence.local_storage as local_storage
 import persistence.google_drive_client as google_storage
+import lib.utils as utils
+
+utils.print_run_configs()
 
 subject = artist_generator.fetch_random_artist()
 image_generation_prompt = ai_client.generate_image_generation_prompt(subject)
+file_name = ai_client.generate_file_name(image_generation_prompt, subject)
 summary = ai_client.generate_subject_summary(subject)
 title = ai_client.generate_image_title(image_generation_prompt)
 image_url = ai_client.generate_and_save_image(image_generation_prompt, subject)
 instagram_caption = helper.build_caption(title, image_generation_prompt, summary)
-
-instagram_client.post_file_to_instagram_with_caption(image_url, instagram_caption)
-file_name = ai_client.generate_file_name(image_generation_prompt, subject)
-upscaler.upscale_and_overwrite_image(file_name)
 local_storage.save_content(subject, title, summary, file_name)
-google_storage.upload_file_to_drive(file_name)
+
+if utils.should_post_to_instagram():
+    instagram_client.post_file_to_instagram_with_caption(image_url, instagram_caption)
+
+if utils.should_upscale_image():
+    upscaler.upscale_and_overwrite_image(file_name)
+
+if utils.should_persist_image_remotely():
+    google_storage.upload_file_to_drive(file_name)
+
+if not utils.should_persist_image_locally():
+    print(f"PERSIST LOCALLY: {utils.should_persist_image_locally()}")
+    utils.clean_up_local_image(file_name)
 
 print(f"Completed all tasks")
